e9407e6a737ec74edebf7a91cb645a7a3bf2a192
Tarik Nasuhoglu <tnasuh@gmail.com>
2022-08-23 10:44:48 +0300

    Initial Commit

---
+++ Spotify/SpotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyApp.swift
+//  Spotify
+//
+//  Created by Tarik Nasuhoglu on 23.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
