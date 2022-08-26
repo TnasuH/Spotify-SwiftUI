6d6ba06a7899deb16b0ae420a15efa275d3de851
Tarik Nasuhoglu <tnasuh@gmail.com>
2022-08-26 10:36:27 +0300

    added trackRow check the has a preview

--- Spotify/Views/Other/TrackListView.swift
+++ Spotify/Views/Other/TrackListView.swift
@@ -18,8 +18,10 @@ struct TrackListView: View {
         ForEach(tracks, id: \.id) {
             item in
             LazyVStack {
+                
                 trackRow(track: item)
                     .onTapGesture {
+                        guard let _ = item.previewurl else { return }
                         var item = item
                         if(item.album == nil) {
                             item.album = self.album
@@ -29,37 +31,50 @@ struct TrackListView: View {
                         pm.isPlaying = true
                         pm.showLargePlayer = true
                     }
+                
+                
             }
         }
     }
     
     private func trackRow(track: Track) -> some View {
-        
+        let isNotPreview = track.previewurl == nil
         return HStack (alignment: .center) {
-            AsyncImage(url: URL(string: track.album?.images.first?.url ?? (album?.images.first?.url ?? ""))) { image in
-                image.resizable()
-                
-            } placeholder: {
-                ProgressView()
+            ZStack {
+                AsyncImage(url: URL(string: track.album?.images.first?.url ?? (album?.images.first?.url ?? ""))) { image in
+                    image.resizable()
+                    
+                } placeholder: {
+                    ProgressView()
+                }
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 40, height: 40, alignment: .center)
+                .cornerRadius(0)
+                if isNotPreview {
+                    Color.black
+                        .opacity(0.5)
+                        .frame(width: 40, height: 40, alignment: .center)
+                }
             }
-            .aspectRatio(contentMode: .fill)
-            .frame(width: 40, height: 40, alignment: .center)
-            .cornerRadius(0)
-            
             VStack(alignment: .leading) {
                 Text(track.name)
                     .font(.headline)
+                    .lineLimit(1)
                 Text(Helper.getArtistsName(artists: track.artists ?? []))
                     .font(.subheadline)
-            }
+                    .lineLimit(1)
+            }.foregroundColor(isNotPreview ?
+                              Color.primary
+                .opacity(0.5) : Color.primary
+            )
             Spacer()
             Image(systemName: "heart.fill")
-                .foregroundColor(.green)
+                .foregroundColor(isNotPreview ? .green.opacity(0.5) : .green)
                 .padding(.trailing,20)
             Image(systemName: "ellipsis")
+                .foregroundColor(isNotPreview ? .primary.opacity(0.5) : .primary)
         }
     }
-    
 }
 
 struct TrackListView_Previews: PreviewProvider {
